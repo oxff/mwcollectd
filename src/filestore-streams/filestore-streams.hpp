2d5780b4cb4c930692cb8111f3bdbfb03a005c3e
Georg Wicherski <gw@mwcollect.org>
2009-05-19 13:40:57 +0000

    filestore-streams:  - Save streams to a file as raw data blobs (TODO: add fuzzy hash fn)

---
+++ src/filestore-streams/filestore-streams.hpp
@@ -0,0 +1,57 @@
+/*
+ *				    _ _           _      _ 
+ *	 _ __ _____      _____ ___ | | | ___  ___| |_ __| |
+ *	| '_ ` _ \ \ /\ / / __/ _ \| | |/ _ \/ __| __/ _` |
+ *	| | | | | \ V  V / (_| (_) | | |  __/ (__| || (_| |
+ *	|_| |_| |_|\_/\_/ \___\___/|_|_|\___|\___|\__\__,_|
+ *
+ *
+ * 	Copyright 2009 Georg Wicherski, Kaspersky Labs GmbH
+ *
+ *
+ *	This file is part of mwcollectd.
+ *
+ *	mwcollectd is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Lesser General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	mwcollectd is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public License
+ *	along with mwcollectd. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#ifndef __MWCOLLECTD_FILESTORESTREAMS_HPP
+#define __MWCOLLECTD_FILESTORESTREAMS_HPP
+
+#include <mwcollectd.hpp>
+using namespace mwcollectd;
+
+class FileStoreStreamsModule : public Module, public EventSubscriber
+{
+public:
+	FileStoreStreamsModule(Daemon * daemon);
+	virtual ~FileStoreStreamsModule() { }
+
+	virtual bool start(Configuration * moduleConfiguration);
+	virtual bool stop();
+
+	virtual const char * getName() { return "filestore-streams"; }
+	virtual const char * getDescription() { return "Save attack stream "
+		"data in your local filesystem."; }
+	virtual void handleEvent(Event * event);
+
+private:
+	Daemon * m_daemon;
+
+	string m_directory;
+};
+
+
+#endif // __MWCOLLECTD_FILESTORESTREAMS_HPP
