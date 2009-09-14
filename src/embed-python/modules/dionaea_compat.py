99a35544305160edaa8fd6ac22c3fb18b16ac7ba
Georg Wicherski <oxff@leviathan.home.oxff.net>
2009-09-14 13:54:39 +0200

    embed-python/dionaea_compat:   - fix syntax error (too early today) -.-

--- src/embed-python/modules/dionaea_compat.py
+++ src/embed-python/modules/dionaea_compat.py
@@ -1,17 +1,17 @@
 from mwcollectd import *
 
 class Logger(object):
-	def __init__(self, prefix):
-		self.prefix = prefix
+	def __init__(self, self.prefix):
+		self.prefix = self.prefix
 
 	def debug(self, msg, *args, ** kwargs):
-		log(L_SPAM, '[Py/' + prefix + '] ' + (msg % args))
+		log(L_SPAM, '[Py/' + self.prefix + '] ' + (msg % args))
 
 	def warning(self, msg, *args, ** kwargs):
-		log(L_INFO, '[Py/' + prefix + '] ' + (msg % args))
+		log(L_INFO, '[Py/' + self.prefix + '] ' + (msg % args))
 
 	def info(self, msg, *args, ** kwargs):
-		log(L_INFO, '[Py/' + prefix + '] ' + (msg % args))
+		log(L_INFO, '[Py/' + self.prefix + '] ' + (msg % args))
 	
 	def critical(self, msg, *args, ** kwargs):
-		log(L_CRIT, '[Py/' + prefix + '] ' + (msg % args))
+		log(L_CRIT, '[Py/' + self.prefix + '] ' + (msg % args))
