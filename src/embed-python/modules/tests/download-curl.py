b9eb89a13503e1ebf759b075b4814b77803f0f92
Georg Wicherski <gw@mwcollect.org>
2010-05-04 17:16:15 +0200

    embed-python:   - add tests/download-curl.py for testing downloads

---
+++ src/embed-python/modules/tests/download-curl.py
@@ -0,0 +1,34 @@
+from mwcollectd import *
+
+class Connection(NetworkEndpoint):
+	def connectionEstablished(self):
+		self.timeouts.sustain = 5
+		self.buffer = b''
+
+		log(L_INFO, 'New curl shell connection, timeout after %is idle time.' % self.timeouts.sustain)
+
+	def connectionClosed(self):
+		pass
+
+	def dataRead(self, buf):
+		self.buffer += buf
+		pos = self.buffer.find(b'\n')
+
+		if pos >= 0:
+			log(L_SPAM, 'Download URL \"%s\".' % self.buffer[:pos].decode('utf8'))
+			dispatchEvent('shellcode.download', { 'url': self.buffer[:pos].decode('utf8'), 'recorder': self.getRecorder() } )
+			self.buffer = self.buffer[pos+1:]
+
+
+def start():
+	global server
+	server = NetworkServer(('any', 8080), Connection)
+
+	return True
+
+
+def stop():
+	global server
+	server.close()
+
+	return True
