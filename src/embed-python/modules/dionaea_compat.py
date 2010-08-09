8336320bde054e6c9d536fc0dad5c074e1ed836c
Mark Schloesser <ms@mwcollect.org>
2010-08-09 18:43:45 +0200

    embed-python: - merge recent changes from dionaea - improve dionaea_compat layer, minor fixes, add buffering - fix file upload, including file url - remove various debug prints

--- src/embed-python/modules/dionaea_compat.py
+++ src/embed-python/modules/dionaea_compat.py
@@ -5,23 +5,60 @@ class Logger(object):
 		self.prefix = prefix
 
 	def debug(self, msg, *args, ** kwargs):
-		mwcollectd.log(mwcollectd.L_SPAM, '[Py/' + self.prefix + '] ' + (msg % args))
+		mwcollectd.log(mwcollectd.L_SPAM, '[Py/' + self.prefix + '] ' + msg + ' '.join(args))
 
 	def warning(self, msg, *args, ** kwargs):
-		mwcollectd.log(mwcollectd.L_INFO, '[Py/' + self.prefix + '] ' + (msg % args))
+		mwcollectd.log(mwcollectd.L_INFO, '[Py/' + self.prefix + '] ' + msg + ' '.join(args))
+
+	def warn(self, *args, ** kwargs):
+		self.warning(*args, **kwargs)
 
 	def info(self, msg, *args, ** kwargs):
-		mwcollectd.log(mwcollectd.L_INFO, '[Py/' + self.prefix + '] ' + (msg % args))
+		mwcollectd.log(mwcollectd.L_INFO, '[Py/' + self.prefix + '] ' + msg + ' '.join(args))
 	
 	def critical(self, msg, *args, ** kwargs):
-		mwcollectd.log(mwcollectd.L_CRIT, '[Py/' + self.prefix + '] ' + (msg % args))
+		mwcollectd.log(mwcollectd.L_CRIT, '[Py/' + self.prefix + '] ' + msg + ' '.join(args))
+
+conn_logger = Logger('connection compat')
 
 class connection(mwcollectd.NetworkEndpoint):
 	def __init__(self, conntype):
 		mwcollectd.NetworkEndpoint.__init__(self)
+		self.buf = b''
 
 	def connectionClosed(self):
-		self.connectionLost()
+		self.handle_disconnect()
 
 	def processors(self):
 		pass
+
+	def dataRead(self, data):
+		r = self.handle_io_in(self.buf + data)
+		self.buf = (self.buf + data)[r:]
+		if len(self.buf) > 1024**2:
+			self.close()
+
+	def connectionEstablished(self):
+		self.handle_established()
+
+	def close(self):
+		conn_logger.debug('close called')
+		mwcollectd.NetworkEndpoint.close(self)
+
+incident_logger = Logger('incident compat')
+
+class incident:
+	def __init__(self, path):
+		self.path = path
+		self.dict = {}
+
+	def __setattr__(self, name, value):
+		self.dict[name] = value
+
+	def __getattr__(self, name):
+		return self.dict[name]
+
+	def report(self):
+		incident_logger.debug('incident reported: path %s dict %s' % (self.path, str(self.dict)))
+
+
